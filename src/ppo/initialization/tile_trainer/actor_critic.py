e2b7b3276f2a9891ab69324f9b3852a1cd376b64
Aayush <aayushnj@gmail.com>
2024-11-02 15:28:22 -0700

    created actor and critic for tile, unet implemented

--- src/ppo/initialization/tile_trainer/actor_critic.py
+++ src/ppo/initialization/tile_trainer/actor_critic.py
@@ -1,39 +1,127 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch import Tensor, optim
 import torch.distributions as dist
 from src.ppo.base_policy import Actor, Critic
+from src.ppo.base_env import Env
+from src.modules.unet import UNet
 
-class LRCritic(Critic):
-    def __init__(self):
+class TileActor(Actor):
+    def __init__(
+        self, 
+        train_env: Env = None, 
+        test_env: Env = None, 
+        in_size = (128, 128, 3),
+        h_dim: int = 64,
+        num_tiles_h: int = 4, 
+        num_tiles_w: int = 4,
+        num_bins: int = 10,
+        device="cuda",
+    ):
         super().__init__()
-        self.network = torch.Sequential(
-            nn.Linear(1, 1),
-            nn.Sigmoid(),
+        self.h, self.w, self.in_channels = in_size
+        self.num_bins = num_bins
+        self.tiles_h = num_tiles_h
+        self.tiles_w = num_tiles_w
+        self.device = device
+        
+        assert self.h % num_tiles_h == 0, "Height must be divisible by num_tiles_h"
+        assert self.w % num_tiles_w == 0, "Width must be divisible by num_tiles_w"
+        assert num_tiles_h == num_tiles_w, "Only square tiles supported for now"
+        
+        maxpool_factor = self.h // num_tiles_h
+        self.unet = UNet(in_size=in_size, h_dim=h_dim, num_bins=self.num_bins) #output: (128, 128, num_bins)
+        self.layers = [
+            self.unet,
+            nn.MaxPool2d(maxpool_factor), #(128, 128, 3) -> (n_tiles, n_tiles, 3)
+            nn.GELU(),
+        ]
+        self.network = nn.Sequential(*self.layers).to(device)
+        self.train_env = train_env
+        self.test_env = test_env
+        
+        self.set_env(train_mode=True)
+
+    def set_env(self, train_mode: bool):
+        assert train_mode or self.test_env is not None, "Test env must be set if train mode is False"
+        self.env = self.train_env if train_mode else self.test_env
+        
+    def forward(self, obs: Tensor):
+        obs = obs.to(self.device)
+        return self.network(obs)
+
+    def actor_distribution(self, obs: Tensor):
+        """
+        Returns
+        -------
+        dist: A flattened Categorical distribution over the bins for each tile
+        """
+        out = self.forward(obs) # (batch_size, n_tiles, n_tiles, num_bins)
+        out = out.view(-1, self.num_bins) # (batch_size * n_tiles * n_tiles, num_bins)
+        out_probs = dist.Categorical(out)
+        # out_probs = out_probs.view(-1, self.num_tiles_h, self.num_tiles_w, self.num_bins)
+        return out_probs
+    
+    def select_action(self, obs: Tensor):
+        actor_dist = self.actor_distribution(obs) # (batch_size * n_tiles_h * n_tiles_w, 1)
+        batch_weights_flat = actor_dist.sample() # (batch_size * n_tiles_h * n_tiles_w, 1)
+        batch_weights = batch_weights.view(-1, self.num_tiles_h, self.num_tiles_w, self.num_bins) # (batch_size, n_tiles, n_tiles, num_bins)
+        log_probs = self.log_prob(actor_dist, batch_weights_flat) # (batch_size, n_tiles, n_tiles, 1)
+
+        return batch_weights, log_probs
+        
+    def log_prob(self, actor_dist, flat_actions):
+        batch_log_probs_flat = actor_dist.log_prob(flat_actions) # (batch_size * n_tiles_h * n_tiles_w, 1)
+        batch_log_probs = batch_log_probs_flat.view(-1, self.num_tiles_h, self.num_tiles_w, 1) # (batch_size, n_tiles, n_tiles, 1)
+        
+        return batch_log_probs
+    
+    def evaluate_actions(
+        self, 
+        obs: Tensor,
+        actions: Tensor # (batch_size, n_tiles, n_tiles, 1)
+    ):
+        actor_dist = self.actor_distribution(obs) # (batch_size * n_tiles_h * n_tiles_w, 1)
+        actions_flat = actions.view(-1, 1) # (batch_size * n_tiles_h * n_tiles_w, 1)
+
+        batch_log_probs = self.log_prob(actor_dist, actions_flat) # (batch_size, n_tiles, n_tiles, 1)
+        
+        flat_entropy = actor_dist.entropy() # (batch_size * n_tiles_h * n_tiles_w, 1)
+        batch_entropy = flat_entropy.view(-1, self.num_tiles_h, self.num_tiles_w, 1) # (batch_size, n_tiles, n_tiles, 1)
+    
+        return batch_log_probs, batch_entropy
+    
+
+class TileCritic(Critic):
+    def __init__(self, train_env: Env, test_env: Env, unet: UNet, input_dim: int = 1024, h_dim: int = 64):
+        super().__init__()
+        # should map obs (image) to value
+        self.actor_encoder = unet.encoder # (batch_size, 8, 8, 4*h_dim)
+        self.linear1 = nn.Linear(4 * unet.h_dim, h_dim) # (batch_size, 8, 8, 4*h_dim) --> (batch_size, h_dim)
+        self.linear2 = nn.Linear(h_dim, 1) # (batch_size, h_dim) --> (batch_size, 1)
+        self.network = nn.Sequential(
+            self.actor_encoder,
+            nn.GELU(),
+            self.linear1,
+            self.GELU(),
+            self.linear2
         )
+        self.train_env = train_env
+        self.test_env = test_env
+        
+        self.set_env(train_mode=True)
+    
+    def set_env(self, train_mode: bool):
+        assert train_mode or self.test_env is not None, "Test env must be set if train mode is False"
+        self.env = self.train_env if train_mode else self.test_env
 
-    def forward(self, action: int, state=None):
-        action = torch.tensor([[action]], dtype=torch.float32)
-        return self.network(action).squeeze(-1)
+    def forward(self, obs: Tensor):        
+        obs = obs.to(torch.int)
+        
+        # Ensure obs is properly reshaped for the network
+        batch_size = obs.shape[0] if len(obs.shape) > 3 else 1
+        # obs = obs.view(batch_size, -1)  # Flatten input to (batch_size, features)
 
-class LRActor(Actor):
-    def __init__(self):
-        super().__init__()
-        self.logits = nn.Parameter(torch.zeros(10))
-        self.lrs = [10**-i for i in range(10)]
-
-    def forward(self):
-        return nn.Softmax(self.logits, dim=-1)
-
-    def select_action(self):
-        log_probs = self.forward()
-        actor_dist = dist.Categorical(logits=log_probs)
-        lr_idx = actor_dist.sample()
-        return self.lrs[lr_idx], actor_dist.log_probs[lr_idx]
-
-    def log_prob(self, obs, acts):
-        log_probs = self.forward()
-        actor_dist = dist.Categorical(logits=log_probs)
-        return log_probs[acts]
-
+        return self.network(obs)
--- src/ppo/initialization/tile_trainer/env.py
+++ src/ppo/initialization/tile_trainer/env.py
@@ -6,30 +6,87 @@ from abc import ABC, abstractmethod
 from src.ppo.tile_trainer import TileTrainer
 from src.ppo.base_env import Env
 
-class Tiled2DEnv(Env):
+import numpy as np
+import torch
+import json
+import warnings
+import os
+from collections import defaultdict
+from examples.image_fitting import SimpleTrainer
+from src.ppo.base_env import Env
+
+from src.utils import image_path_to_tensor, dino_preprocess
+
+
+class Tile2DEnv(Env):
     """
     A test environment where the agent's actions are used to simulate 
     the process of training a neural network.
     """
-
-    def __init__(self, img, num_points: int, iterations: int, lr: float):
+    def __init__(
+        self, 
+        lr: float,
+        dataset_path: str,
+        num_points: int, 
+        num_iterations: int,
+        # TODO: remove observation_shape?
+        observation_shape: tuple, 
+        action_shape: tuple,
+        num_trials: int = 1,
+        device='cuda',
+        num_tiles_h: int = 2,
+        num_tiles_w: int = 2,
+    ):
         # Environment state would be the 2d image
         # action: tile weights
         # 
         self.max_steps = 1
-
         self.num_points = num_points
-        self.iterations = iterations
+        self.num_trials = num_trials
+        self.num_iterations = num_iterations
+        
         self.lr = lr
-    
+
+        self.device = device
+        # self.observation_shape = img.shape
+        self.action_shape = action_shape
+
+        self.observation_shape = (1,) # Just img index
+
+        self.num_imgs = 0
+
+        self.imgs = []
+        self.img_names = []
+        
+        for img_name in os.listdir(dataset_path):
+            self.num_imgs += 1
+            full_path = os.path.join(dataset_path, img_name)
+            img = image_path_to_tensor(full_path)
+            self.imgs.append(img)
+            self.img_names.append(img_name)
+            
+        print("=" * 100)    
+        print(f'num images: {self.num_images}\n num_trials: {self.num_trials}\n num_points: {self.num_points}\n num_iterations: {self.num_iterations}, lr: {self.lr}')
+        print("=" * 100)    
+        self.imgs = torch.stack(self.imgs)
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        dataset_name = dataset_path.split('/')[-1]
+        
+        self.sample_new_img()
+
+    def sample_new_img(self):
+        self.current_img_idx = torch.tensor(np.random.randint(0, self.num_images), device=self.device)
+
     def reset(self):
         """
         Reset the environment to an initial state.
         """
-        
-        return self.img
+        # Randomly select new img idx
+        self.sample_new_img()
+        return self.get_observation()
 
-    def step(self, action: list[float]):
+    def step(self, action: int):
         """
         Simulate applying the action (inputs to a neural network) and return 
         the next state, reward, and whether the episode is done.
@@ -42,25 +99,37 @@ class Tiled2DEnv(Env):
             reward: The reward, which could be based on the network performance.
             done: A boolean indicating if the episode has ended.
         """
+        # unsqueeze action if 0-d tensor
+        if len(action.shape) == 0:
+            action = action.unsqueeze(-1)
+            
+        # Convert actions to int to use as indices
+        actions_int = action.long()
+        batch_psnr = self.psnr[self.current_img_idx][actions_int]
+
+        # mse_diff = batch_losses[:, 0] - batch_losses[:, -1]
+        # Using PSNR as reward (TODO: normalize for critic?)
+        # print(f"batch_losses: {reward_psnr} for action: {action}")
+
+        # torch bool
+        done = torch.as_tensor(True, device=self.device, dtype=torch.bool)
         
-        trainer = TileTrainer(
-            gt_image=self.img,
-            num_points=self.num_points,
-            tile_weights=action
-        )
-        losses, _ = trainer.train(
-            iterations=self.iterations,
-            lr=self.lr,
-            save_imgs=True,
-            model_type='3dgs',
-        )
-        reward = losses[0] - losses[-1]
-        done = True
-        
-        return self.get_observation(), reward, done
+        # print(f"for action: {action}, reward: {reward}, using lr: {self.lrs[int(action.item())]}")
+        return self.get_observation(), batch_psnr, done
 
     def get_observation(self):
         """
-        Return the current state
+        Return the current state (img_idx)
         """
-        return self.img
+        return self.current_img_idx
+    
+    def get_encoded_images(self, img_idx: torch.tensor):
+        imgs = self.encoded_images[img_idx]
+        if len(imgs.shape) == 4:
+            imgs = imgs.squeeze(1)
+        if len(imgs.shape) == 3:
+            imgs = imgs.squeeze(1)
+        return imgs
+    
+    def get_mean_reward(self, img_idx: torch.tensor):
+        return self.psnr_stats['mean'][img_idx]
